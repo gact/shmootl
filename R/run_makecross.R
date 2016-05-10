3f6bf0662fb2eae9cac505cc5def8ff3fbbcfa34
tw164 <tw164@le.ac.uk>
2016-05-10 16:37:30 +0100

    Add pipeline makecross

---
+++ R/run_makecross.R
@@ -0,0 +1,28 @@
+# Start of run_makecross.R #####################################################
+
+# run_makecross ----------------------------------------------------------------
+#' Run run_makecross pipeline.
+#' 
+#' @description This script combines separate genotype and 
+#' phenotype files into a single cross data file.
+#' 
+#' @param genfile input genotype CSV file
+#' @param phefile input phenotype CSV file
+#' @param crossfile output cross CSV file
+#' 
+#' @export
+#' @rdname run_makecross
+run_makecross <- function(genfile, phefile, crossfile) {
+    
+    geno <- readGenoCSV(genfile)
+    
+    pheno <- readPhenoCSV(phefile)
+    
+    cross <- makeCross(geno, pheno)
+    
+    writeCrossCSV(cross, crossfile)
+    
+    return( invisible() )
+}
+
+# End of run_makecross.R #######################################################
