9c24104c00d38c0cfcfd59f92d12783e6fea7f91
tw164 <tw164@le.ac.uk>
2016-06-08 12:30:05 +0100

    Add pullmap pipeline

---
+++ R/run_pullmap.R
@@ -0,0 +1,61 @@
+# Start of run_pullmap.R #######################################################
+
+# run_pullmap ------------------------------------------------------------------
+#' Pull map from cross/geno data.
+#' 
+#' @description This script pulls the map from an R/qtl cross or genotype file,
+#' and writes it to a separate map file.
+#' 
+#' @param infile input cross/geno CSV file
+#' @param mapfile output map CSV file
+#' @param require.mapunit require map units in input
+#' @param include.mapunit include map units in output
+#' 
+#' @export
+#' @rdname run_pullmap
+run_pullmap <- function(infile, mapfile, require.mapunit=TRUE,
+    include.mapunit=TRUE) {
+    
+    stopifnot( isSingleString(mapfile) )
+    
+    guess <- sniffCSV(infile)
+    
+    if ( is.null(guess) ) {
+        stop("cannot pull map - unknown input data")
+    }
+    
+    if ( ! hasMapCSV(infile) ) {
+        stop("no map data found in file '", infile,"'")
+    }
+    
+    if ( guess == 'cross' ) {
+        
+        cross <- readCrossCSV(infile, require.mapunit=require.mapunit)
+        cross.map <- qtl::pull.map(cross)
+        
+    } else if ( guess == 'geno' ) {
+        
+        geno <- readGenoCSV(infile, require.mapunit=require.mapunit)
+        
+        cross.info <- attr(geno, 'info')
+        samples <- getSamples(cross.info)
+        pheno <- makePlaceholderPheno(samples=samples)
+        
+        cross <- makeCross(geno, pheno)
+        cross.map <- qtl::pull.map(cross)
+        
+    } else if ( guess == 'map' ) {
+        
+        cross.map <- readMapCSV(infile, require.mapunit=require.mapunit)
+        
+    } else {
+        
+        stop("cannot pull map from ", guess," data")
+    }
+    
+    writeMapCSV(cross.map, mapfile, include.mapunit=include.mapunit)
+    
+    return( invisible() )
+}
+
+# End of run_pullmap.R #########################################################
