3af97cc2f45a1cc7b41b503fdbfdffe58d46e287
tw164 <tw164@le.ac.uk>
2016-09-02 14:10:38 +0100

    Simplify check for enumerated genotypes

--- R/hasEnumGenotypes.R
+++ R/hasEnumGenotypes.R
@@ -17,53 +17,9 @@ hasEnumGenotypes <- function(x) {
 #' @export
 #' @rdname hasEnumGenotypes
 hasEnumGenotypes.cross <- function(x) {
-    
     stopifnot( 'cross' %in% class(x) )
     stopifnot( attr(x, 'crosstype') == 'bc' ) # TODO: support other cross types
-    
-    # Get cross alleles.
-    alleles <- attr(x, 'alleles')
-    
-    # Check alleles are for enumerated genotypes.
-    if ( ! all( isEnumAllele(alleles) ) ) {
-        return(FALSE)
-    }
-    
-    # Check alleles are consecutive integers starting from 1.
-    allele.numbers <- sort( as.integer(alleles) )
-    if ( allele.numbers[1] != 1 || any( diff(allele.numbers) != 1 ) ) {
-        return(FALSE)
-    }
-    
-    # Drop markers that don't have at least one genotype.
-    x <- qtl::drop.nullmarkers(x)
-    
-    # Get genotype matrix.
-    geno.matrix <- qtl::pull.geno(x)
-    
-    # Replace encoded genotypes with actual genotype values.
-    for ( i in getIndices(alleles) ) {
-        geno.matrix[ geno.matrix == i ] <- alleles[i]
-    }
-    
-    # Get column indices of genotype matrix.
-    cols <- getColIndices(geno.matrix)
-    
-    # Get row indices of first genotype values.
-    rows <- sapply(cols, function(j) which( ! is.na(geno.matrix[, j]) )[1])
-    
-    # Get first genotype value for each marker.
-    prime.geno <- sapply(getIndices(cols), function(i) geno.matrix[rows[i], cols[i]])
-    
-    prime.geno <- unique(prime.geno)
-    
-    # If there is not a unique first genotype with the
-    # symbol '1', this is not enumerated genotype data.
-    if ( length(prime.geno) != 1 || prime.geno != '1' ) {
-        return(FALSE)
-    }
- 
-    return(TRUE)   
+    return( all( isEnumAllele( attr(x, 'alleles') ) ) )
 }
 
 # End of hasEnumGenotypes.R ####################################################
